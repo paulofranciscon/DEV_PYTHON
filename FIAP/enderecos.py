a5338d8a370124604e7aca3f5b516157be664d30
Paulo Franciscon <57269765+paulofranciscon@users.noreply.github.com>
2021-05-27 17:05:22 -0300

    Update enderecos.py

--- FIAP/enderecos.py
+++ FIAP/enderecos.py
@@ -3,8 +3,8 @@ from copy import copy
 from pygeocoder import Geocoder
 
 endereco = (input('Digite o endereço: '))
-resultado = Geocoder('AIzaSyDsF6-ETfWjCOTIxbwc6cnINqx8MNAeJho').geocode(endereco)
-coordinates = Geocoder('AIzaSyDsF6-ETfWjCOTIxbwc6cnINqx8MNAeJho').geocode(endereco).coordinates
+resultado = Geocoder('key API').geocode(endereco)
+coordinates = Geocoder('key API').geocode(endereco).coordinates
                                                                 #Access reverse to coordinates
 #coordinates2 = Geocoder('AIzaSyDsF6-ETfWjCOTIxbwc6cnINqx8MNAeJho').reverse_geocode (-22.1984674, -49.9564299)
 
